badcf0f528c712752e92981b8ddc62128993dcd6
Franck Jullien <franck.jullien@gmail.com>
2020-03-01 17:57:41 +0100

    Add recorder module

---
+++ pcie_analyzer/recorder/recorder.py
@@ -0,0 +1,115 @@
+# This file is Copyright (c) 2020 Franck Jullien <franck.jullien@gmail.com>
+# License: BSD
+
+from migen import *
+
+from litex.soc.interconnect.csr import *
+from litex.soc.interconnect import stream
+
+from litedram.frontend.dma import LiteDRAMDMAWriter
+
+import sys
+sys.path.append("..")
+from common import *
+
+# *********************************************************
+# *                                                       *
+# *                     Recorder                          *
+# *                                                       *
+# *********************************************************
+
+class RingRecorder(Module, AutoCSR):
+    def __init__(self, dram_port, base, length):
+        self.start    = CSR()             # Start recorder
+        self.stop     = CSR()             # Stop recorder
+        self.finished = CSRStatus()       # Capture finished
+        self.size     = CSRStorage(32)    # Post trigger size
+        self.offset   = CSRStorage(32)    # Trigger offset
+
+        self.trigAddr = Signal(32)
+
+        self.source = source = stream.Endpoint([("address", dram_port.address_width),
+                                                ("data", dram_port.data_width)])
+        self.sink   = sink   = stream.Endpoint(trigger_layout)
+
+        # # #
+
+        addr  = Signal(32)
+        count = Signal(32)
+
+        self.submodules.stride = stream.StrideConverter(trigger_layout, recorder_layout, reverse=False)
+        self.submodules.fifo   = ResetInserter()(stream.SyncFIFO(recorder_layout, 1024, buffered=True))
+
+        self.comb += [
+            sink.connect(self.stride.sink, omit={"valid"}),
+            self.stride.source.connect(self.fifo.sink),
+            source.address.eq(addr),
+            source.data.eq(self.fifo.source.payload.raw_bits()),
+        ]
+
+        # FSM
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
+
+        fsm.act("IDLE",
+            self.fifo.reset.eq(1),
+            NextValue(addr, base),
+            NextValue(count, 0),
+            If(self.start.re,
+                NextValue(self.finished.status, 0),
+                NextValue(self.stride.sink.valid, 1),
+                NextState("FILL_PRE_TRIG")
+            )
+        )
+
+        fsm.act("FILL_PRE_TRIG",
+            source.valid.eq(self.fifo.source.valid),
+            self.fifo.source.ready.eq(source.ready),
+            If(source.valid & source.ready,
+                NextValue(addr, addr + 1),
+                NextValue(count, count + 1),
+                If(count == self.offset.storage,
+                    NextValue(count, 0),
+                    NextState("WAIT_TRIGGER")
+                )
+            )
+        )
+
+        fsm.act("WAIT_TRIGGER",
+            source.valid.eq(self.fifo.source.valid),
+            self.fifo.source.ready.eq(source.ready),
+            If(source.valid & source.ready,
+                NextValue(addr, addr + 1),
+                If(addr == (base + length - 1),
+                    NextValue(addr, base),
+                ),
+                If(self.fifo.source.trig != 0,
+                    NextValue(self.trigAddr, addr),
+                    NextState("FILL_POST_TRIG")
+                )
+            )
+        )
+
+        fsm.act("FILL_POST_TRIG",
+            source.valid.eq(self.fifo.source.valid),
+            self.fifo.source.ready.eq(source.ready),
+            If(source.valid & source.ready,
+                NextValue(addr, addr + 1),
+                If(addr == (base + length - 1),
+                    NextValue(addr, base),
+                ),
+                NextValue(count, count + 1),
+                If(count == self.size.storage,
+                    NextState("DONE")
+                )
+            )
+        )
+
+        fsm.act("DONE",
+            NextValue(self.stride.sink.valid, 0),
+            NextValue(self.finished.status, 1),
+            If(self.stop.re,
+
+                NextState("IDLE")
+            )
+        )
+
---
+++ pcie_analyzer/recorder/test_recorder.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2020 Franck Jullien <franck.jullien@gmail.com>
+# License: BSD
+
+import csv
+
+from migen import *
+from migen.fhdl import *
+
+from litex.soc.interconnect.stream import *
+from litex.soc.interconnect.stream_sim import *
+from litex.soc.interconnect import csr
+from litex.soc.interconnect import csr_bus
+
+from recorder import *
+
+import sys
+sys.path.append("..")
+from common import *
+
+# *********************************************************
+# *                                                       *
+# *                      Helpers                          *
+# *                                                       *
+# *********************************************************
+
+def make_data_stride(dataList):
+    data = 0
+    ctrl = 0
+    trig = 0
+    for _trig, _ctrl, _data in dataList:
+        data = (data << 16) + _data
+        ctrl = (ctrl << 2) + _ctrl
+        trig = (trig << 1) + _trig
+    retval = (trig << 216) + (ctrl << 192) + data
+    #print("{:64x}".format(retval))
+    #print("{:3x} {:6x} {:48x}".format(trig, ctrl, data))
+    return retval
+
+def make_data(trig, ctrl, data):
+    return (trig << 18) + (ctrl << 16) + data
+
+# *********************************************************
+# *                                                       *
+# *                  Simulation datas                     *
+# *                                                       *
+# *********************************************************
+
+# data ---------------------+
+# ctrl --------------+      |
+# trig----------+    |      |
+#               |    |      |
+#               v    v      v
+data_stride = [
+    make_data_stride([(1, 0b10, 0xAA00),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (1, 0b01, 0x00BB)]),
+    make_data_stride([(1, 0b10, 0xAA00),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (0, 0b00, 0x0000),
+                      (1, 0b01, 0x00BB)]),
+]
+
+# data ----------------+
+# ctrl ----------+     |
+# trig -------+  |     |
+#             |  |     |
+#             v  v     v
+data = [
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 3, 0x1111),
+    make_data(0, 3, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+    make_data(0, 0, 0x1111),
+
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 1, 0x2222),
+    make_data(0, 3, 0x2222),
+    make_data(0, 3, 0x2222),
+    make_data(0, 3, 0x2222),
+    make_data(0, 0, 0x2222),
+    make_data(0, 0, 0x2222),
+
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 1, 0x3333),
+    make_data(0, 3, 0x3333),
+    make_data(0, 3, 0x3333),
+    make_data(0, 3, 0x3333),
+    make_data(0, 0, 0x3333),
+    make_data(0, 0, 0x3333),
+
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 1, 0x4444),
+    make_data(0, 3, 0x4444),
+    make_data(0, 3, 0x4444),
+    make_data(0, 3, 0x4444),
+    make_data(0, 0, 0x4444),
+    make_data(0, 0, 0x4444),
+
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 1, 0x5555),
+    make_data(0, 3, 0x5555),
+    make_data(0, 3, 0x5555),
+    make_data(0, 3, 0x5555),
+    make_data(0, 0, 0x5555),
+    make_data(0, 0, 0x5555),
+
+    make_data(1, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 1, 0x6666),
+    make_data(0, 3, 0x6666),
+    make_data(0, 3, 0x6666),
+    make_data(0, 3, 0x6666),
+    make_data(0, 0, 0x6666),
+    make_data(0, 0, 0x6666),
+
+]
+
+data_list = []
+
+def fill_data_list():
+    for i in range(800):
+        trig = 0
+        if i == 180:
+            trig = 1
+        data_list.append(make_data(trig, 0b11, i))
+
+# *********************************************************
+# *                                                       *
+# *                     Test bench                        *
+# *                                                       *
+# *********************************************************
+
+class DummyPort():
+    def __init__(self, aw, dw):
+        self.address_width = aw
+        self.data_width = dw
+
+class TB(Module):
+    def __init__(self):
+
+        port = DummyPort(32, 256)
+
+        RING_BUFFER_BASE_ADDRESS = 0x1000
+        RING_BUFFER_SIZE         = 0x10
+
+        self.submodules.streamer = PacketStreamer(trigger_layout)
+        self.submodules.recorder = RingRecorder(port, RING_BUFFER_BASE_ADDRESS, RING_BUFFER_SIZE)
+
+        self.comb += [
+            self.streamer.source.connect(self.recorder.sink),
+        ]
+
+# *********************************************************
+# *                                                       *
+# *                         Main                          *
+# *                                                       *
+# *********************************************************
+
+def main_generator(dut):
+
+    fill_data_list()
+
+    packet = Packet(data_list)
+    dut.streamer.send(packet)
+
+    yield from dut.recorder.offset.write(4)
+
+    delay = 0
+
+    for i in range(1000):
+        if i == 20:
+            yield from dut.recorder.start.write(1)
+            
+        if (i > 150) and (i < 200):
+            yield dut.recorder.source.ready.eq(0)
+        else:
+            yield dut.recorder.source.ready.eq(1)
+
+        if (yield dut.recorder.finished.status):
+            delay = delay + 1
+            if (delay == 100):
+                yield from dut.recorder.stop.write(1)
+            if (delay == 150):
+                yield from dut.recorder.start.write(1)
+
+        yield
+
+# *********************************************************
+# *                                                       *
+# *                   Run simulation                      *
+# *                                                       *
+# *********************************************************
+
+if __name__ == "__main__":
+    tb = TB()
+    generators = {
+        "sys" :   [main_generator(tb),
+                   tb.streamer.generator()]
+    }
+    clocks = {"sys": 10}
+
+    run_simulation(tb, generators, clocks, vcd_name="sim.vcd")
