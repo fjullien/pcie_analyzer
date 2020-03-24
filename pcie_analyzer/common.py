05d3be7867f4e6b973541caa2b47f83838bcd57e
Franck Jullien <franck.jullien@gmail.com>
2020-03-24 20:16:17 +0100

    common: add some definitions

--- pcie_analyzer/common.py
+++ pcie_analyzer/common.py
@@ -3,6 +3,36 @@
 
 from enum import IntEnum
 
+def K(x, y):
+    """K code generator ex: K(28, 5) is COM Symbol"""
+    return (y << 5) | x
+
+def D(x, y):
+    """D code generator"""
+    return (y << 5) | x
+
+# Symbols (6.3.5) ----------------------------------------------------------------------------------
+
+class Symbol:
+    """Symbol definition with name, 8-bit value and description"""
+    def __init__(self, name, value, description=""):
+        self.name        = name
+        self.value       = value
+        self.description = description
+
+SKP =  Symbol("SKP", K(28, 0), "Skip")
+COM =  Symbol("COM", K(28, 5), "Comma")
+STP =  Symbol("STP", K(27, 7), "Start TLP")
+SDP =  Symbol("SDP", K(28, 2), "Start DLLP")
+END =  Symbol("END", K(29, 7), "End")
+EDB =  Symbol("EDB", K(30, 7), "End Bad")
+PAD =  Symbol("PAD", K(23, 7), "Padding")
+FTS =  Symbol("FTS", K(28, 1), "Fast Train Sequence")
+IDL =  Symbol("IDL", K(28, 3), "Electrical Idle")
+EIE =  Symbol("EIE", K(28, 7), "Electrical Idle Exit")
+
+symbols = [SKP, COM, STP, SDP, END, EDB, PAD, FTS, IDL, EIE]
+
 class osetsType(IntEnum):
     DATA = 0
     SKIP = 1
@@ -25,6 +55,17 @@ trigger_layout = [
     ("ctrl"   , 2),  # 2 K symbols flag
     ("trig"   , 1)   # 1 Trigger position indicator
 #   ("spare"  , 1)   # 
+]
+
+gtp_layout = [
+    ("data"   , 16), # 2 bytes
+    ("ctrl"   , 2),  # 2 K symbols flag
+]
+
+filter_layout = [
+    ("data"   , 16), # 2 bytes
+    ("ctrl"   , 2),  # 2 K symbols flag
+    ("time"   , 1)   # 1 Trigger position indicator
 ]
 
 def recorder_layout(nb):
