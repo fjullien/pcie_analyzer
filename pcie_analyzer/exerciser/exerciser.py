6def63835d76532eaef1d3eadc5b040653208b0e
Franck Jullien <franck.jullien@gmail.com>
2020-04-16 22:10:48 +0200

    Add exerciser

---
+++ pcie_analyzer/exerciser/exerciser.py
@@ -0,0 +1,131 @@
+# This file is Copyright (c) 2020 Franck Jullien <franck.jullien@gmail.com>
+# License: BSD
+
+from migen import *
+from migen.genlib.cdc import *
+
+from litex.soc.interconnect.csr import *
+from litex.soc.interconnect import stream
+
+import sys
+sys.path.append("..")
+from common import *
+sys.path.append("../descrambler")
+from descrambler import *
+
+class Exerciser(Module, AutoCSR):
+    def __init__(self, clock_domain, mem_size=128):
+
+        # *********************************************************
+        # *                    Interface                          *
+        # *********************************************************
+        self.insert = CSRStorage()
+        self.size   = CSRStorage(8)   # Pattern size - 1
+
+        self.source = source = stream.Endpoint(gtp_layout)
+
+        # *********************************************************
+        # *                      Signals                          *
+        # *********************************************************
+        symbol_cnt  = Signal(16)
+        free_cnt    = Signal(32)
+        memory_cnt  = Signal(8)
+        done        = Signal()
+
+        _insert     = Signal()
+        _size       = Signal(8)
+
+        generator   = stream.Endpoint(descrambler_layout)
+
+        # *********************************************************
+        # *                         CDC                           *
+        # *********************************************************
+        self.specials += MultiReg(self.insert.storage, _insert, clock_domain)
+        self.specials += MultiReg(self.size.storage, _size, clock_domain)
+
+        # *********************************************************
+        # *                     Specials                          *
+        # *********************************************************
+        self.specials.mem = Memory(20, mem_size)
+        self.specials.rdport = self.mem.get_port(write_capable=False, async_read=True, clock_domain=clock_domain)
+
+        # *********************************************************
+        # *                     Submodules                        *
+        # *********************************************************
+
+        self.submodules.descrambler = Descrambler(clock_domain)
+
+        # *********************************************************
+        # *                    Combinatorial                      *
+        # *********************************************************
+        self.comb += [
+            generator.connect(self.descrambler.sink),
+            self.source.data.eq(Cat(self.descrambler.source.data[8:16], self.descrambler.source.data[0:8])),
+            self.source.ctrl.eq(Cat(self.descrambler.source.ctrl[1], self.descrambler.source.ctrl[0])),
+            self.source.valid.eq(1),
+        ]
+
+        self.sync += [
+                If(_insert == 0,
+                    done.eq(0),
+                )
+        ]
+
+        # *********************************************************
+        # *                        FSM                            *
+        # *********************************************************
+        fsm = FSM(reset_state="LSB")
+        self.submodules.fsm = ClockDomainsRenamer(clock_domain)(fsm)
+
+        fsm.act("LSB",
+            NextValue(symbol_cnt, symbol_cnt + 1),
+            NextValue(generator.data, free_cnt[15:32]),
+            NextValue(generator.ctrl, 0),
+            NextValue(generator.osets, 0),
+            NextState("MSB"),
+        )
+
+        fsm.act("MSB",
+            NextValue(symbol_cnt, symbol_cnt + 1),
+            NextValue(free_cnt, free_cnt + 1),
+            NextValue(generator.data, free_cnt[0:16]),
+            NextValue(generator.ctrl, 0),
+            NextState("LSB"),
+            If(symbol_cnt > 100,
+                NextState("SKIP_LSB"),
+            ).Else(
+                NextState("LSB"),
+            ),
+            If(_insert & ~done,
+                self.rdport.adr.eq(0),
+                NextValue(memory_cnt, 0),
+                NextState("SEND_FRAME"),
+            )
+        )
+
+        fsm.act("SKIP_LSB",
+            NextValue(generator.data, 0xbc1c),
+            NextValue(generator.ctrl, 0b11),
+            NextValue(generator.osets, 0b11),
+            NextState("SKIP_MSB"),
+        )
+
+        fsm.act("SKIP_MSB",
+            NextValue(symbol_cnt, 0),
+            NextValue(generator.data, 0x1c1c),
+            NextValue(generator.ctrl, 0b11),
+            NextValue(generator.osets, 0b11),
+            NextState("LSB"),
+        )
+
+        fsm.act("SEND_FRAME",
+            NextValue(memory_cnt, memory_cnt + 1),
+            self.rdport.adr.eq(memory_cnt),
+            NextValue(generator.data, self.rdport.dat_r[0:16]),
+            NextValue(generator.ctrl, self.rdport.dat_r[16:18]),
+            NextValue(generator.osets, self.rdport.dat_r[18:20]),
+            If(memory_cnt == (_size - 1),
+                NextValue(done, 1),
+                NextState("LSB"),
+            ),
+        )
---
+++ pcie_analyzer/exerciser/test_exerciser.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2020 Franck Jullien <franck.jullien@gmail.com>
+# License: BSD
+
+import csv
+
+from migen import *
+from migen.fhdl import *
+
+from litex.soc.interconnect.stream import *
+from litex.soc.interconnect.stream_sim import *
+
+from exerciser import *
+
+sys.path.append("../descrambler")
+from descrambler import *
+
+# *********************************************************
+# *                                                       *
+# *                      Helpers                          *
+# *                                                       *
+# *********************************************************
+
+def make_mem_data(dc, ctrl, data):
+    return (dc << 18) + (ctrl << 16) + data
+
+# *********************************************************
+# *                                                       *
+# *                  Simulation datas                     *
+# *                                                       *
+# *********************************************************
+
+#  data       ---------------------------------+
+#  ctrl       --------------------------+      |
+#  osets      ----------------------+   |      |
+#  address    --+                   |   |      |
+#               |                   |   |      |
+#               v                   v   v      v
+mem_data =    [(0, make_mem_data(0b00, 0b00, 0x1122)),
+               (1, make_mem_data(0b00, 0b00, 0x3344)),
+               (2, make_mem_data(0b00, 0b00, 0x5566)),
+               (3, make_mem_data(0b00, 0b00, 0x7788)),
+]
+
+# *********************************************************
+# *                                                       *
+# *                     Test bench                        *
+# *                                                       *
+# *********************************************************
+
+class TB(Module):
+    def __init__(self):
+
+        self.submodules.exerciser = Exerciser("sys")
+        self.submodules.descrambler_test = Descrambler("sys")
+        self.submodules.detect = DetectOrderedSets()
+
+        self.specials.wrport = self.exerciser.mem.get_port(write_capable=True, clock_domain="sys")
+
+        self.comb += [
+            self.detect.sink.data.eq(self.exerciser.source.data),
+            self.detect.sink.ctrl.eq(self.exerciser.source.ctrl),
+            self.detect.sink.valid.eq(self.exerciser.source.valid),
+
+            self.descrambler_test.sink.valid.eq(1),
+            self.descrambler_test.sink.data.eq(self.detect.source.data),
+            self.descrambler_test.sink.ctrl.eq(self.detect.source.ctrl),
+            self.descrambler_test.sink.osets.eq(self.detect.source.osets),
+            self.descrambler_test.sink.type.eq(self.detect.source.type),
+            self.descrambler_test.source.ready.eq(1),
+        ]
+
+# *********************************************************
+# *                                                       *
+# *                         Main                          *
+# *                                                       *
+# *********************************************************
+
+def main_generator(dut):
+
+    # Fill insert memory
+    for (addr, dat) in mem_data:
+        yield dut.wrport.adr.eq(addr)
+        yield dut.wrport.dat_w.eq(dat)
+        yield dut.wrport.we.eq(1)
+        yield
+    yield dut.wrport.we.eq(0)
+
+    # Configure insert size
+    yield from dut.exerciser.size.write(len(mem_data))
+
+    for i in range(400):
+        if (i == 180):
+            yield from dut.exerciser.insert.write(1)
+        if (i == 250):
+            yield from dut.exerciser.insert.write(0)
+        yield
+
+# *********************************************************
+# *                                                       *
+# *                   Run simulation                      *
+# *                                                       *
+# *******************************************************
+
+if __name__ == "__main__":
+    tb = TB()
+    generators = {
+        "sys" :   [main_generator(tb)]
+    }
+    clocks = {"sys": 10}
+
+    run_simulation(tb, generators, clocks, vcd_name="sim.vcd")
